dc50c2853e9863305dc587a849a461d912e076fe
JorgeFigueroa626 <jlfigueroa626@gmail.com>
2024-11-18 19:40:31 -0400

    Se agrego la API

---
+++ src/main/java/posBlogPosts/PosBlogPostsApplication.java
@@ -0,0 +1,13 @@
+package posBlogPosts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PosBlogPostsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PosBlogPostsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/posBlogPosts/PosBlogPostsApplicationTests.java
@@ -0,0 +1,13 @@
+package posBlogPosts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PosBlogPostsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
